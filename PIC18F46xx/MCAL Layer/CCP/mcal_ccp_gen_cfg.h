0296e39d36ccf86d9abdf15a59679b1ac7e5f670
AmrAli47 <167882152+AmrAli47@users.noreply.github.com>
2024-06-03 02:04:15 +0300

    CCP driver

--- PIC18F46xx/MCAL Layer/CCP/mcal_ccp_gen_cfg.h
+++ PIC18F46xx/MCAL Layer/CCP/mcal_ccp_gen_cfg.h
@@ -8,7 +8,7 @@
 #ifndef MCAL_CCP_GEN_CFG_H
 #define	MCAL_CCP_GEN_CFG_H
 
-#define Capture_Mode 1
+#define Capture_Mode 1 
 #define Compare_Mode 2
 #define PWM_Mode     3
 #define CCP_Module_Works_in Capture_Mode
